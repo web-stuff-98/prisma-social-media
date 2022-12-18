8bb2561ca9e91925155c99dbc58aa407315601e6
web-stuff-98 <jphoneacc98@gmail.com>
2022-12-18 05:27:52 +0000

    i think that fixed it

--- dist/frontend/src/components/ErrorTip.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-function ErrorTip({ message }) {
-    return ((0, jsx_runtime_1.jsx)("div", Object.assign({ className: "bg-rose-700 rounded px-2 leading-5", style: { position: "absolute", left: "0.666rem", top: "1.33rem" } }, { children: message })));
-}
-exports.default = ErrorTip;
