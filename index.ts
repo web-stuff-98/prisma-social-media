54ac146198f0e836a7001129ea346163d0e5d4d0
web-stuff-98 <jphoneacc98@gmail.com>
2022-12-18 04:09:04 +0000

    moved server files to root because heroku wouldnt build it otherwise

--- index.ts
+++ index.ts
@@ -40,10 +40,10 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../..", "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
   app.get("*", (_, res) => {
     res.sendFile(
-      path.join(__dirname, "../..", "frontend", "build", "index.html")
+      path.join(__dirname, "..", "frontend", "build", "index.html")
     );
   });
   //seed();
